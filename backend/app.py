c319cc334316c91feb14925b14e6d8efb4f5c6a5
Nathan Santiago <santiagonathan9254@gmail.com>
2024-11-02 22:50:43 -0400

    Create components.

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 # external
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # internal
@@ -30,6 +31,19 @@ async def lifespan(app: FastAPI):
     motor_client.close()
 
 app: FastAPI = FastAPI(lifespan=lifespan)
+
+# Configure CORS
+origins = [
+    "http://localhost:3000",  # Your frontend's origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(database_router)
 app.include_router(spoonacular_router)
