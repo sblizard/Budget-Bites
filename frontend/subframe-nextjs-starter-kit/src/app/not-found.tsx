6e938eb85f4ad0c98da16c947033f9b1acdc778a
Sean Blizard <emailseanblizard@gmail.com>
2024-11-02 23:36:47 -0400

    not found page created

---
+++ frontend/subframe-nextjs-starter-kit/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function notFound() {
+    return (
+        <div className="flex w-full h-full items-center justify-center">
+            <span className="text-heading-1 font-heading-1 text-default-font">
+                404 Not Found
+            </span>
+        </div>
+    );
+}
